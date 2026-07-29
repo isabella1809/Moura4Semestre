a42613bdb464c30174f7de3736c2d20088c8c609
ISABELLA SANTOS GARCIA <56436641866@info.local>
2026-07-29 16:49:06 -0300

    primeira atividade do 4semestre

---
+++ reactlist/src/App.jsx
@@ -0,0 +1,69 @@
+
+import { useState } from 'react'
+import './App.css'
+import penIcon from "./assets/vector.svg"
+import trashIcon from "./assets/vectorcc.svg"
+
+function App() {
+  //States
+  const [tasklist, SetTasklist] = useState([
+    { id: 1, description: "Revisar HTML" },
+    { id: 2, description: "Revisar CSS" },
+    { id: 3, description: "Revisar Reactjs" },
+    { id: 4, description: "Revisar React Native" },
+  ])
+  //Effects
+  //funções
+
+  return (
+    <>
+      <header className='header-section'>
+
+        <h1 className='header-section__title'>
+          React List
+        </h1>
+      </header>
+      <main className='body-section'>
+
+        <form className='cad-task'>
+          <input
+            className='card-task__entry'
+            type="text"
+            placeholder='adicione uma tarefa' />
+          <button className='car-task__btn-confirm' >Adicionar</button>
+        </form>
+
+        <section className='cardlist'>
+
+          {
+            tasklist.map((t) => {
+              return (
+                <article className='cardtask' key={t.id}>
+
+                  <p className='cardtask__task-text'>{t.description}</p>
+                  <div className='cardtask__icon-box'>
+                    <div className='cardlist__icon'>
+                      <img src={penIcon}
+                        className='cardList__edit-icon' alt="" />
+                    </div>
+                    <div className='cardlist__icon'>
+                      <img src={trashIcon} className='cardList__delet-icon' alt="" />
+                    </div>
+                  </div>
+
+                </article>
+              );
+            })
+          }
+
+
+        </section>
+      </main>
+      <footer className='footer-list'>
+        <p className='footer-list__right-text'>2026, React List - todos os direitos reservados</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
